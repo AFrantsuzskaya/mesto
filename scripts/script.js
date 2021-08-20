02d41d2f5a1dee9d0d8a48e9bb17d0d72df83527
AFrantsuzskaya <anastasiafrantsuzskaya@yandex.ru>
2021-08-20 14:22:11 +0300

    feat: финальная версия для ревью 2

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let openPopupButton = document.querySelector('.profile__edit-button'); 
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__occupation');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__field_type_mame');
+let jobInput = document.querySelector('.popup__field_type_about-me');
+let submitButton = document.querySelector('.popup__submit-button');
+
+function togglePopup() {
+  if (!popup.classList.contains('popup_open')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
+  
+   popup.classList.toggle('popup_open');
+}
+
+openPopupButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', togglePopup);
+
+function formSubmitHandler (event) {
+    event.preventDefault();
+    
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
+submitButton.addEventListener('click', togglePopup);
